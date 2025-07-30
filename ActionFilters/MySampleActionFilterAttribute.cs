518c4273d541a1e86d7bbeb7b4d0505c9bf21d71
Shahzaib Ahmed Khan <shahzaib.ahmed@hyperdatacomputing.com>
2025-07-30 20:05:52 +0500

    complete Action Filters

--- ActionFilters/MySampleActionFilterAttribute.cs
+++ ActionFilters/MySampleActionFilterAttribute.cs
@@ -6,13 +6,18 @@ namespace ActionFilters
     /// <summary>
     /// Attribute now I can make it an attribute
     /// </summary>
-    public class MySampleActionFilterAttribute : Attribute , IActionFilter
+    public class MySampleActionFilterAttribute : Attribute, IActionFilter, IOrderedFilter
     {
         private string _name;
-        public MySampleActionFilterAttribute(string name)
+        public MySampleActionFilterAttribute(string name, int order = 0)
         {
             _name = name;
+            Order = order;
         }
+
+        //IOrderedFilter  
+        public int Order { get; set; }
+
         public void OnActionExecuted(ActionExecutedContext context)
         {
             Console.WriteLine($"OnActionExecuted - {_name}");
